5c6fd5b4a05e39bed68cfd1a86822ca4e2eb583a
darkatak <darkatak@hotmail.com>
2024-06-05 20:46:44 +0200

    First version

---
+++ remote_nrpe_host/openshift.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#set -x
+ALERTNAME=$1
+PROMETHEUS_ENDPOINT=192.168.1.58:8081/api/v1/alerts
+
+CURL_STATUS=`curl --write-out %{http_code} --output /dev/null --silent -k $PROMETHEUS_ENDPOINT`
+if [[ "$CURL_STATUS" -ne 200 ]];then
+   echo ERROR - Connectivity to Openshift Prometheus lost.
+   exit 2
+fi
+
+CHECK="`curl -k $PROMETHEUS_ENDPOINT | jq -r '.data.alerts[].labels.alertname' | grep -i $ALERTNAME`"
+
+if [ "$CHECK" == "$ALERTNAME" ];then
+   if [ "$ALERTNAME" == "Watchdog" ];then
+          echo Watchdog alert found. Prometheus is OK
+          exit 0
+  else
+	  SEVERITY=`curl -k $PROMETHEUS_ENDPOINT | jq -r --arg ALERTNAME "$ALERTNAME" '.data.alerts[]|select(.labels.alertname as $alertname|.labels.alertname|contains($ALERTNAME))'|jq -r '.labels.severity'`
+          echo $SEVERITY - $1
+          exit 2
+   fi
+ else
+   echo OK - $1 Status OK
+   exit 0
+fi
