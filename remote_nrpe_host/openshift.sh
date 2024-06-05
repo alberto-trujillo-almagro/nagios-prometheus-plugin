fbb03a962f00f3a024ec0e60fe895d20a579810c
darkatak <darkatak@hotmail.com>
2024-06-05 21:20:39 +0200

    Add conversion from prometheus severity to nagios

--- remote_nrpe_host/openshift.sh
+++ remote_nrpe_host/openshift.sh
@@ -3,6 +3,21 @@
 ALERTNAME=$1
 PROMETHEUS_ENDPOINT=192.168.1.58:8081/api/v1/alerts
 
+severity_2_nagios()
+{
+case $1 in
+ "critical")
+     SEVERITY_CODE=2
+ ;;
+ "warning")
+     SEVERITY_CODE=1
+ ;;
+ *)
+     SEVERITY_CODE=3
+ ;;
+esac
+}
+
 CURL_STATUS=`curl --write-out %{http_code} --output /dev/null --silent -k $PROMETHEUS_ENDPOINT`
 if [[ "$CURL_STATUS" -ne 200 ]];then
    echo ERROR - Connectivity to Openshift Prometheus lost.
@@ -17,8 +32,9 @@ if [ "$CHECK" == "$ALERTNAME" ];then
           exit 0
   else
 	  SEVERITY=`curl -k $PROMETHEUS_ENDPOINT | jq -r --arg ALERTNAME "$ALERTNAME" '.data.alerts[]|select(.labels.alertname as $alertname|.labels.alertname|contains($ALERTNAME))'|jq -r '.labels.severity'`
+          severity_2_nagios $SEVERITY
           echo $SEVERITY - $1
-          exit 2
+          exit $SEVERITY_CODE
    fi
  else
    echo OK - $1 Status OK
